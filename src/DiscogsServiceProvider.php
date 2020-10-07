25c60c2d58eda12a25f2cb695bcc78f6d4b60f39
Otar Tsilosani <o.tsilosani@livo.ge>
2020-10-07 14:56:32 +0400

    package discovery setup

---
+++ src/DiscogsServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Otsilosani\DiscogsLaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class DiscogsServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * [register description]
+     *
+     * @return [type] [description]
+     */
+    public function register()
+    {
+        //
+    }
+}
